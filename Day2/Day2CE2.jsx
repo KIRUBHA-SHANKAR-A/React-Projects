185798885ca7a807a734f0793206fa6baea0a705
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2024-08-01 12:19:48 +0530

    Update Day2CE2.jsx

--- Day2/Day2CE2.jsx
+++ Day2/Day2CE2.jsx
@@ -4,7 +4,7 @@ const Second = () =>{
     return(
       <>
           <h1 style={{color:"green"}}>Inline Style in JSX Example.</h1>
-          <div style={{backgroundColor:"lightblue",color:"darkblue",padding:'10px',border : '1 px solid blue', borderRadius : '5px'}}>
+          <div style={{backgroundColor:"lightblue",color:"darkblue",padding:'10px',border : '1px solid blue', borderRadius : '5px'}}>
             <p style ={{color:"darkblue",fontSize:'16px'}} >This is a paragraph with inline styles applied.</p>
             
           </div>
