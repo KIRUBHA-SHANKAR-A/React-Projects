9a2fe156293ad79c9b2b2f66543f378db0eda99c
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2024-08-08 12:32:21 +0530

    Add files via upload

---
+++ Day 1/Day1_PAH1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Team = () =>
+{
+     return (
+        <div>
+             <p>This is Team functional component</p>
+        </div>
+     );
+}
+export default Team;
