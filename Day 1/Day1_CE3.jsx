a9801b1ac270a2aa01fb49ab9a411833b7eaecb0
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2024-08-08 12:31:36 +0530

    Add files via upload

---
+++ Day 1/Day1_CE3.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const World = () =>
+{
+     return (
+        <div>
+             <p>This is World functional component</p>
+        </div>
+     );
+}
+export default World;
