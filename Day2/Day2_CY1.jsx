5055cd32f1c9553d6f303f55fa665bc31cea0f24
KIRUBHA-SHANKAR-A <727723eucs106@skcet.ac.in>
2024-08-09 21:38:59 +0530

    Add files via upload

---
+++ Day2/Day2_CY1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () =>{
+     return(
+        <div style={{textAlign:'center'}}>
+            <style>{'body { background-color: lavender; }'}</style>
+            <h1>Smile Component</h1>
+            <h6>It is a functional component</h6>
+            <img src="https://static.vecteezy.com/system/resources/previews/003/660/834/original/happy-smile-emoticon-expression-free-vector.jpg " alt="" width={'200px'} height={'200px'}/>
+        </div>
+     ); 
+}
+
+export default App;
